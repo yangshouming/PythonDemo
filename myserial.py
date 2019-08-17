c9e683bc7881c47fa8cad313b6b679bf9c7a9ba1
yangshouming <53643832+yangshouming@users.noreply.github.com>
2019-08-17 21:10:44 +0800

    Update myserial.py

--- myserial.py
+++ myserial.py
@@ -1,8 +1,24 @@
 import serial
+import serial.tools.list_ports
 
-sPort = 'COM37'
-sBaudRate = 115200
+sPort = 'COM1'
+sBaudRate = 9600
 sTimeOut = 1
+
+# port_list = list(serial.tools.list_ports.comports())
+# if len(port_list) == 0:
+#     print('找不到串口')
+# else:
+#     for i in range(0, len(port_list)):
+#         print(port_list[i])
+
+# str_com = port_list[1]
+# str_com = str(str_com)
+# sPort = (str_com[0:5])
+sPort.strip()
+
+# sPort = 'COM3'
+
 try:
     myserial = serial.Serial(port=sPort, baudrate=sBaudRate, timeout=sTimeOut)
     print('open serial ok')
@@ -19,4 +35,4 @@ except:
     print('serial_read_data error')
 
 
-print(myserial.read(6))
+# print(myserial.read(6))
