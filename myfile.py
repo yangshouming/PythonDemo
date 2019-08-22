87bf59b4d6791b0e29b790ad8345099b887d06c2
yangshouming <53643832+yangshouming@users.noreply.github.com>
2019-08-22 20:05:24 +0800

    fiel read write
    fiel read write

--- myfile.py
+++ myfile.py
@@ -1,5 +1,16 @@
+
+
+# -*- coding: UTF-8 -*-
+
 import sys
 import os
+sys.stdout.write('stdout write 测试\n')
+
+print('file test=====================================')
+myfile1 = open("myfile1_2019-8-22.txt", 'a+')
+myfile1.write("enter the first text\n")
 
 
-sys.stdout.write('这是一个stdout wrrite 测试')
+myfile1 = open("myfile1_2019-8-22.txt", 'a+')
+file_read_string = myfile1.read()
+print(file_read_string)
